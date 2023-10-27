a6390c5e566dd334f4ac62331307bd3d89f28cbb
Joshua Tazman Reinier <jreinier@oberlin.edu>
2023-10-26 21:58:31 -0400

    enabling local development

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,1 +1,7 @@
-If you would like to contribute to the plugin, post an issue describing your contribution or idea and tag with "contribution." The developer (joshuatazrein) will respond to discuss it. 
+If you would like to contribute:
+1. Submit an issue proposing your changes. 
+2. Fork this repository.
+3. `npm run dev` to develop. When testing changes, just run `cp dist/* "<your Obsidian vault>/plugins/time-ruler"` (recommended to install hot-reload and include a  `.hotreload` file in the `dist` directory).
+4. When finished, please create a Pull Request describing your changes.
+
+Submit an issue on GitHub if you have any questions!
---
+++ tailwind.config.js
@@ -0,0 +1,59 @@
+const plugin = require('tailwindcss/plugin')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [`src/**/*.{tsx,css,ts}`],
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant('child', '& > *')
+      addVariant('ancestor', '& *')
+    }),
+  ],
+  corePlugins: {
+    preflight: false,
+  },
+  important: '#time-ruler',
+  theme: {
+    extend: {
+      minHeight: {
+        line: 'var(--font-text-size)',
+      },
+      lineHeight: {
+        line: 'var(--line-height-normal)',
+      },
+      height: {
+        line: 'calc(var(--line-height-normal) * 1em)',
+      },
+      fontSize: {
+        base: 'var(--font-text-size)',
+      },
+      fontFamily: {
+        menu: 'var(--font-interface)',
+        serif: 'var(--font-text)',
+        sans: 'var(--font-text)',
+      },
+      borderRadius: {
+        icon: 'var(--clickable-icon-radius)',
+        checkbox: 'var(--checkbox-radius)',
+      },
+      colors: {
+        primary: 'var(--background-primary)',
+        'primary-alt': 'var(--background-primary-alt)',
+        secondary: 'var(--background-secondary)',
+        'secondary-alt': 'var(--background-secondary-alt)',
+        error: 'var(--background-modifier-error)',
+        border: 'var(--background-modifier-border)',
+        hover: 'var(--background-modifier-hover)',
+        selection: 'var(--text-selection)',
+        normal: 'var(--text-normal)',
+        muted: 'var(--text-muted)',
+        faint: 'var(--text-faint)',
+        accent: 'var(--text-accent)',
+      },
+      screens: {
+        mobile: { raw: '(hover: none)' },
+        mouse: { raw: '(hover: hover)' },
+      },
+    },
+  },
+}
